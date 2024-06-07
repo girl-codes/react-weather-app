7f48729f8312757ccb8689d726a03bc3df55ace1
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-06-07 12:52:09 +0100

    Added GitHub account and repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/girl-codes"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Komal Ahmed{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/girl-codes/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
