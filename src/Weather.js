cd2b7beeaa4d20785fc9c235d74b5fd3f65e0500
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-06-07 15:02:29 +0100

    Added basic CSS to project

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,17 +30,22 @@ export default function Weather() {
       </ul>
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly cloudy"
-          />
-          18°C
+          <div className="weather-info">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly cloudy"
+            />
+            <div>
+              <span className="temperature">18</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
             <li>Precipitation: 2%</li>
             <li> Humidity: 49% </li>
-            <li> Wind: 10 mph</li>
+            <li> Wind: 10mph</li>
           </ul>
         </div>
       </div>
