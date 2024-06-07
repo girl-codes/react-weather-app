23740c6a03a6b22c9aeaad22c69c2fd359bf74d6
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-06-07 13:16:45 +0100

    Added Bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -5,25 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/girl-codes"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Komal Ahmed{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/girl-codes/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/girl-codes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Komal Ahmed{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/girl-codes/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
