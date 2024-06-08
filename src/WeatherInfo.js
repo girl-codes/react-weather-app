2271b87cd895a39ee3402478739877926ef7d3bb
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-06-08 16:43:32 +0100

    Updated styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,10 +1,11 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
-    <div>
+    <div className="weather-block">
       <h1> {props.data.city} </h1>
       <ul>
         <li>
@@ -12,27 +13,24 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize"> {props.data.description}</li>
       </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="weather-info">
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={55} />
-            </div>
-            <div>
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
-            </div>
-          </div>
+      <div className="weather-info">
+        <div className="weather-icon float-left">
+          {" "}
+          <WeatherIcon code={props.data.icon} size={55} />
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 2%</li>
-            <li> Humidity: {props.data.humidity} % </li>
-            <li> Wind: {Math.round(props.data.wind)} mph</li>
-          </ul>
+        <div className="float-left">
+          {" "}
+          <span className="temperature">
+            {Math.round(props.data.temperature)}
+          </span>
+          <span className="unit">°C</span>
         </div>
+      </div>
+      <div className="temp-info">
+        <ul>
+          <li> Humidity: {props.data.humidity} % </li>
+          <li> Wind: {Math.round(props.data.wind)} mph</li>
+        </ul>
       </div>
     </div>
   );
